f4773c24a26fa209aff245254d777ffc250677e4
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2018-07-09 17:39:57 +0100

    New Problems 2

---
+++ Jhool.java
@@ -0,0 +1,45 @@
+import java.util.* ;
+
+class Jhool{
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in) ;
+        try{
+            String[] inputStr = (input.nextLine().trim().split(""));
+            int[] mainArr = new int[inputStr.length] ;
+            for(int counter=0; counter<inputStr.length ; counter++){
+                mainArr[counter] = Integer.parseInt(inputStr[counter]) ;
+            }
+            int iterator = 0 ;
+            int consecutive  = 0;
+            boolean luck = true ;
+            int currentVar = -1  , prevVar = -1;
+
+            while(iterator < mainArr.length){
+                currentVar = mainArr[iterator] ;
+                if(iterator!=0) {
+                    prevVar = mainArr[iterator - 1];
+
+                    if (prevVar == currentVar) {
+                        consecutive += 1;
+                        if (consecutive >= 6) {
+                            luck = false;
+                        }
+                    } else if (prevVar != currentVar) {
+                        consecutive = 0;
+                    }
+                }
+                iterator+= 1 ;
+            }
+            if(luck){
+                System.out.println("Good luck!") ;
+            }else{
+                System.out.println("Sorry, sorry!") ;
+            }
+
+
+        }catch(NumberFormatException e){
+            System.out.println(e+" is not a number") ;
+        }
+    }
+}
---
+++ TheGreatKian.java
@@ -0,0 +1,39 @@
+import java.util.*  ;
+
+class theGreatKian{
+
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in) ;
+
+        int arrSize = Integer.parseInt(input.nextLine().trim());
+        String[] inputStrs = (input.nextLine().trim().split(" ")) ;
+        if(inputStrs.length!= arrSize){
+            System.out.println("Incorrect size or input array") ;
+            System.exit(1) ;
+        }
+        int[] mainArr = new int[arrSize] ;
+
+        for(int counter = 0 ;counter<inputStrs.length; counter++){
+            mainArr[counter] = Integer.parseInt(inputStrs[counter].trim());
+        }
+        int sum1 = 0 , sum2 = 0 , sum3 = 0 ;
+
+        for(int counter = 0 ; counter<mainArr.length ; counter++){
+            int firstNum = 1 , secondNum = 2 ;
+
+            if(counter+3<mainArr.length){
+                sum1+= mainArr[counter] ;
+            }
+            if(counter+firstNum<mainArr.length){
+                sum2+= mainArr[counter+firstNum] ;
+            }
+            if(counter+secondNum< mainArr.length){
+                sum3 += mainArr[counter+secondNum] ;
+            }
+        }
+        System.out.println(sum1+" "+sum2+" "+sum3) ;
+
+
+    }
+}
