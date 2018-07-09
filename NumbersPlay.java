0207ab1ce2bd6387a6af0b5e012ea3cf3a143247
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2018-07-09 11:06:54 +0100

    New Problems

--- NumbersPlay.java
+++ NumbersPlay.java
@@ -1,2 +1,56 @@
-public class NumbersPlay {
+import java.util.* ;
+
+class NumbersPlay{
+
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in) ;
+
+        String[] details = input.nextLine().trim().split(" ") ;
+
+        try{
+            int numberOfElements = Integer.parseInt(details[0].trim())  ;
+            int numberOfIterations = Integer.parseInt(details[1].trim());
+
+            String[] allIntegers = input.nextLine().trim().split(" ") ;
+            if(allIntegers.length != numberOfElements){
+                System.out.println("Define the array again !") ;
+                System.exit(0) ;
+            }
+            else{
+
+                int[] mainArr = new int[numberOfElements] ;
+                for(int counter = 0 ; counter<mainArr.length ; counter++){
+                    mainArr[counter] = Integer.parseInt(allIntegers[counter]) ;
+                }
+
+                int[] averages = new int[numberOfIterations];
+                int[] indices = new int[2] ;
+                while(numberOfIterations>0){
+
+                    String[] indicesStr = input.nextLine().trim().split(" ") ;
+                    for(int counter = 0 ; counter < indices.length ;counter++){
+                        indices[counter] = Integer.parseInt(indicesStr[counter]) ;
+                    }
+
+                    int leftIndex = indices[0] , rightIndex = indices[1] ;
+                    int sum =0 ;
+                    for(int counter = leftIndex-1 ; counter<=rightIndex-1 ; counter++){
+                        sum+= mainArr[counter] ;
+                    }
+                    averages[numberOfIterations -1]= sum/((rightIndex-leftIndex)+1)  ;
+
+                    numberOfIterations -=1 ;
+                }
+                // reverse loop
+                for(int counter = averages.length -1; counter>=0 ; counter--){
+                    System.out.println(averages[counter]) ;
+                }
+            }
+        }catch(NumberFormatException e){
+            System.out.println(e +" is not a number") ;
+        }
+
+    }
+
 }
---
+++ TwoStrings.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class TwoStrings{
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in) ;
+
+        try{
+            int numberOfTests = Integer.parseInt(input.nextLine().trim()) ;
+            boolean[] answers = new boolean[numberOfTests] ;
+            for(int counter = 0; counter<answers.length ; counter++){
+                answers[counter] = true ;
+            }
+            while(numberOfTests>0){
+
+                String[] inputStrings =input.nextLine().trim().split(" ") ;
+                String original = inputStrings[0] ;
+                String possiblePermutation = inputStrings[1];
+                char[] originalArr = original.toCharArray() ;
+                char[] permutationArr = possiblePermutation.toCharArray() ;
+
+
+
+                // if the length is different .. save computation time
+                if(original.length() != possiblePermutation.length()){
+                    answers[numberOfTests-1] = false ;
+                    continue ;
+                }
+                // sorting original array
+                for(int nextElement =1 ; nextElement<originalArr.length; nextElement++){
+                    char key = originalArr[nextElement] ;
+                    int currentElement =  nextElement -1 ;
+                    while(currentElement>=0 && originalArr[currentElement]>key){
+                        originalArr[currentElement+1] = originalArr[currentElement] ;
+                        currentElement-=1 ;
+                    }
+                    originalArr[currentElement+1] = key ;
+                }
+
+                // sorting the permutation string
+                for(int nextElement = 1 ; nextElement<permutationArr.length;nextElement++){
+                    char key = permutationArr[nextElement] ;
+                    int currentElement = nextElement - 1 ;
+                    while(currentElement>=0 && permutationArr[currentElement]>key){
+                        permutationArr[currentElement+1] = permutationArr[currentElement] ;
+                        currentElement-= 1 ;
+                    }
+                    permutationArr[currentElement+1] = key ;
+                }
+
+
+
+            for(int counter = 0 ; counter<originalArr.length ; counter++){
+                    if(originalArr[counter]!= permutationArr[counter]){answers[numberOfTests-1] = false ; }
+            }
+            numberOfTests-- ;
+
+
+            }
+            /*
+            for(boolean value: answers){
+                if(value){
+                    System.out.println("YES");
+                }
+                else{
+                    System.out.println("NO") ;
+                }
+            } */
+
+            for(int counter = answers.length-1 ; counter>= 0 ; counter--){
+                if(answers[counter]){
+                    System.out.println("YES") ;
+                }
+                else{
+                    System.out.println("NO") ;
+                }
+            }
+        }catch(NumberFormatException e){
+            System.out.println(e+"is not an integer") ;
+        }
+
+    }
+
+
+}
