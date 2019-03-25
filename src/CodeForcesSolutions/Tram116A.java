83192e735f2471982a6a9592aae6b29a4834807f
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2019-03-26 03:09:04 +0800

    Tram116A

---
+++ src/CodeForcesSolutions/Tram116A.java
@@ -0,0 +1,30 @@
+package CodeForcesSolutions;
+
+import java.util.Scanner;
+
+public class Tram116A {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        int numberOfIterations = Integer.parseInt(input.nextLine().trim()) ;
+
+        int currentNumber = 0 , maxNumber = 0 ;
+
+        while(numberOfIterations>0){
+            String[] inputs = input.nextLine().trim().split("\\s") ;
+            int[] inputsNumbers  = new int[inputs.length] ;
+            inputsNumbers[0] = Integer.parseInt(inputs[0]) ;
+            inputsNumbers[1] = Integer.parseInt(inputs[1]) ;
+
+            if(currentNumber> maxNumber){
+                maxNumber = currentNumber ;
+            }
+            currentNumber -= inputsNumbers[0] ;
+            currentNumber += inputsNumbers[1] ;
+
+
+            numberOfIterations-- ;
+        }
+
+        System.out.println(maxNumber);
+    }
+}
