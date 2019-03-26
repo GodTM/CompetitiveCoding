a197b929d3fcd835bc909c6aa2b667dcaf485460
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2019-03-26 11:58:25 +0800

    deleted code A

--- src/CodeForcesSolutions/B/Taxi158B.java
+++ src/CodeForcesSolutions/B/Taxi158B.java
@@ -41,45 +41,6 @@ public class Taxi158B {
             }
             toBeProcessed = Taxi158B.insertionSort(toBeProcessed);
 
-
-
-
-
-            int counter = 0;
-            int sum = 0;
-            int count = 0;
-            /*
-            while (sum <= 4 && counter <= toBeProcessed.length) {
-                if(counter<toBeProcessed.length) {
-                    sum += toBeProcessed[counter];
-                }
-                if (sum == 4) {
-                    count++;
-                    // reset the sum
-                    sum = 0;
-                } else if (sum > 4) {
-                    // increase the count , reset the sum , and then include this entry in a new taxi
-                    count++;
-                    sum = 0;
-                    sum += toBeProcessed[counter];
-                }
-                /*
-                int sum2 = 0 ;
-                if(counter>0) {
-                    sum2 = toBeProcessed[counter] + toBeProcessed[counter - 1];
-                }
-                if(counter == toBeProcessed.length-1 && sum2>=4 &&(toBeProcessed[counter-1]!=4)){
-                    count++ ;
-                    sum= 0 ;
-                    sum+= toBeProcessed[counter];
-                }
-                */
-            /*
-                counter++;
-            }
-            */
-
-
             int HighCounter , LowCounter , Counter   , SpaceLeft;
 
             for(HighCounter= inStrs.length-1 , Counter=0 ,  LowCounter= 0 ; LowCounter<= HighCounter;){
