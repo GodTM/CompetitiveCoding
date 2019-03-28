be44f16bc4dedac044dfe1cf14c0f3a8a59a9920
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2019-03-29 03:47:32 +0800

    266A added

--- src/CodeForcesSolutions/A/StonesOnTable266A.java
+++ src/CodeForcesSolutions/A/StonesOnTable266A.java
@@ -2,35 +2,24 @@ package CodeForcesSolutions.A;
 
 import java.util.LinkedList;
 import java.util.Scanner;
-/*
+
 public class StonesOnTable266A {
     public static int count = 0 ;
     public static void main(String args[]){
         Scanner input = new Scanner(System.in) ;
+        int size = Integer.parseInt(input.nextLine().trim()) ;
         String[] inputStrArr = input.nextLine().trim().split("") ;
-        LinkedList<String> stringLinkedList = {}
-        for(int counter = 0; counter< inputStrArr.length ; counter++){
-            if(counter!= inputStrArr.length-1){
-                if(inputStrArr[counter].equals(inputStrArr[counter+1])){
-                    count+=1 ;
+        if(size== inputStrArr.length && size>=1 && size<=50) {
+            for (int counter = 0; counter < inputStrArr.length; counter++) {
+                if (counter != inputStrArr.length - 1) {
+                    if (inputStrArr[counter].equals(inputStrArr[counter + 1])) {
+                        count += 1;
+                    }
                 }
             }
-            else{
 
-            }
-
-
-//        /
-/
-            /
-
+            System.out.println(count);
         }
-
-
-
-
-
 
     }
 }
-*/
